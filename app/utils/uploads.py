12e7956a877c5c983112382a48755634d05d42e1
ven-62 <152240057+ven-62@users.noreply.github.com>
2026-03-09 23:43:15 +0800

    fix filename error

--- app/utils/uploads.py
+++ app/utils/uploads.py
@@ -4,18 +4,18 @@ from cloudinary.utils import cloudinary_url
 
 from config import Config
 
-# Configuration       
-cloudinary.config( 
-    cloud_name = Config.CLOUDINARY_CLOUD_NAME, 
-    api_key = Config.CLOUDINARY_API_KEY, 
-    api_secret = Config.CLOUDINARY_SECRET_KEY, # Click 'View API Keys' above to copy your API secret
-    secure=True
+# Configuration
+cloudinary.config(
+    cloud_name=Config.CLOUDINARY_CLOUD_NAME,
+    api_key=Config.CLOUDINARY_API_KEY,
+    api_secret=Config.CLOUDINARY_SECRET_KEY,  # Click 'View API Keys' above to copy your API secret
+    secure=True,
 )
 
-def upload_file(file_path, public_id=None):
+
+def upload_file(file_path):
     """Uploads a file to Cloudinary."""
-    if public_id:
-        result = cloudinary.uploader.upload(file_path, public_id=public_id, use_filename=True, resource_type="auto")
-    else:
-        result = cloudinary.uploader.upload(file_path, use_filename=True, resource_type="auto")
-    return result
+    return cloudinary.uploader.upload(
+        file_path, use_filename=True, resource_type="auto"
+    )
+
